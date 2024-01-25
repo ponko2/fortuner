643ceb0970195e525652493b0dce8fca287ddc36
Takahito Nakano <kano@ponko2.jp>
2024-01-25 19:22:08 +0900

    Chapter 12.2

---
+++ mk-dat.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+DIR="./tests/inputs"
+cd $DIR
+rm -f ./*.dat
+
+for FILE in *; do
+  if [[ -f $FILE ]]; then
+    echo "$FILE"
+    strfile -c % "$FILE" "$FILE.dat" >/dev/null
+  fi
+done
+
+echo "Done."
