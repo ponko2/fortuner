286300c7b7a374d8e33dcca144d5509977b2fd37
Takahito Nakano <kano@ponko2.jp>
2024-01-28 16:13:47 +0900

    Chapter 12.2.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use anyhow::{anyhow, Result};
 use clap::Parser;
 use regex::{Regex, RegexBuilder};
+use std::path::PathBuf;
 
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
@@ -44,6 +45,63 @@ pub fn get_args() -> Result<Config> {
 }
 
 pub fn run(config: Config) -> Result<()> {
-    dbg!(config);
+    let files = find_files(&config.sources)?;
+    dbg!(files);
     Ok(())
+}
+
+fn find_files(paths: &[String]) -> Result<Vec<PathBuf>> {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::find_files;
+
+    #[test]
+    fn test_find_files() {
+        // 存在するファイルを検索できることを確認する
+        let res = find_files(&["./tests/inputs/jokes".to_string()]);
+        assert!(res.is_ok());
+
+        let files = res.unwrap();
+        assert_eq!(files.len(), 1);
+        assert_eq!(
+            files.first().unwrap().to_string_lossy(),
+            "./tests/inputs/jokes"
+        );
+
+        // 存在しないファイルの検索には失敗する
+        let res = find_files(&["/path/does/not/exist".to_string()]);
+        assert!(res.is_err());
+
+        // 拡張子が「.dat」以外の入力ファイルをすべて検索する
+        let res = find_files(&["./tests/inputs".to_string()]);
+        assert!(res.is_ok());
+
+        // ファイル数とファイルの順番を確認する
+        let files = res.unwrap();
+        assert_eq!(files.len(), 5);
+        let first = files.first().unwrap().display().to_string();
+        assert!(first.contains("ascii-art"));
+        let last = files.last().unwrap().display().to_string();
+        assert!(last.contains("quotes"));
+
+        // 複数のソースに対するテストをする。
+        // パスは重複なしでソートされた状態でなければならない
+        let res = find_files(&[
+            "./tests/inputs/jokes".to_string(),
+            "./tests/inputs/ascii-art".to_string(),
+            "./tests/inputs/jokes".to_string(),
+        ]);
+        assert!(res.is_ok());
+        let files = res.unwrap();
+        assert_eq!(files.len(), 2);
+        if let Some(filename) = files.first().unwrap().file_name() {
+            assert_eq!(filename.to_string_lossy(), "ascii-art".to_string())
+        }
+        if let Some(filename) = files.last().unwrap().file_name() {
+            assert_eq!(filename.to_string_lossy(), "jokes".to_string())
+        }
+    }
 }
